c203c1bbb88289d76190f871f722784666393763
ThijsDrost <120206360+ThijsDrost@users.noreply.github.com>
2025-07-22 10:05:34 +0200

    Add NoValue to __all__

--- checking/__init__.py
+++ checking/__init__.py
@@ -4,7 +4,7 @@ from ._descriptors import Descriptor
 from ._validators import Validator
 from .number_line import Range, NumberLine
 
-__all__ = []
+__all__ = [NoValue]
 __all_exports = [ValidatorError, Descriptor, Validator, Range, NumberLine]
 
 for _e in __all_exports:
